b5002be23d055f6e97d6606074a63d57b8cbf830
Minh Thong <lmthong97@gmail.com>
2022-03-01 08:06:41 +0700

    Add axios - Add axiosClient - Add postApi

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
+console.log('hello from main.js')
+
+async function main() {
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  }
+
+  const response = await postApi.getAll(queryParams)
+  console.log(response)
+}
+main()
