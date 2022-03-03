f9f9e1ab12e41ce867e6397c157172cd845859bf
Minh Thong <lmthong97@gmail.com>
2022-03-03 08:58:14 +0700

    get form values when submit

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -9,9 +9,39 @@ function setFormValue(form, formValue) {
   setBackgroundImage(document, '#postHeroImage', formValue?.imageUrl)
 }
 
+function getFormValues(form) {
+  const formValues = {}
+
+  // S1: query each input and add to formValues object
+  //   ;['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //     const field = form.querySelector(`[name="${name}"]`)
+  //     if (field) formValues[name] = field.value
+  //   })
+
+  // S2: using form data
+  const data = new FormData(form)
+  for (const [key, value] of data) {
+    formValues[key] = value
+  }
+
+  return formValues
+}
+
 export function initPostForm({ formId, defaultValue, onSubmit }) {
   const form = document.getElementById(formId)
   if (!form) return
 
   setFormValue(form, defaultValue)
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    //get form values
+    const formValues = getFormValues(form)
+    console.log(formValues)
+
+    //validation
+    //if valid trigger submit callback
+    //otherwise, show validation errors
+  })
 }
