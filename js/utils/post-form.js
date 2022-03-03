7caea0c5bd88593f9fa3293654a8cdbc8f36d6d4
Minh Thong <lmthong97@gmail.com>
2022-03-04 04:47:46 +0700

    validate custom

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,4 +1,4 @@
-import { setBackgroundImage, setFieldValue } from './common'
+import { setBackgroundImage, setFieldValue, setTextContent } from './common'
 
 function setFormValue(form, formValue) {
   setFieldValue(form, '[name="title"]', formValue?.title)
@@ -27,6 +27,46 @@ function getFormValues(form) {
   return formValues
 }
 
+function getTitleError(form) {
+  const titleElement = form.querySelector('[name="title"]')
+  if (!titleElement) return
+  //required
+  if (titleElement.validity.valueMissing) return 'Please enter title.'
+
+  //at least 2 words
+  if (
+    titleElement.value.split(' ').filter((x) => !!x && x.length > 2).length < 2
+  ) {
+    return 'Please enter at least two words of 3 characters'
+  }
+  return ''
+}
+
+function validatePostForm(form, formValues) {
+  //get errors
+  const errors = {
+    title: getTitleError(form),
+    //author: getAuthorError(form),
+    //...
+  }
+
+  //set errors
+  for (const key in errors) {
+    const element = form.querySelector(`[name="${key}"]`)
+
+    if (element) {
+      element.setCustomValidity(errors[key])
+      setTextContent(element.parentElement, '.invalid-feedback', errors[key])
+    }
+  }
+
+  //add was-validated class to form element
+  const isValid = form.checkValidity()
+  if (!isValid) form.classList.add('was-validated')
+
+  return isValid
+}
+
 export function initPostForm({ formId, defaultValue, onSubmit }) {
   const form = document.getElementById(formId)
   if (!form) return
@@ -43,5 +83,6 @@ export function initPostForm({ formId, defaultValue, onSubmit }) {
     //validation
     //if valid trigger submit callback
     //otherwise, show validation errors
+    if (!validatePostForm(form, formValues)) return
   })
 }
